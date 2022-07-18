5013819f871a78ae09620f7a8888b184dcec6c35
Abhi <patelabhi585@gmail.com>
2022-07-18 12:49:43 -0400

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/index.html'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/shopping-cart/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/index.html'));
+});
+app.listen(process.env.PORT || 8004);
+
+
